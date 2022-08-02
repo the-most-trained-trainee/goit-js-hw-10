bbf2cd7955a91c4f60b005d004e220c275fdeca5
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-02 23:51:28 +0300

    Interim Commit 02082022

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+import debounce from 'lodash.debounce'
+
+const DEBOUNCE_DELAY = 900;
+
+const countryInput = document.querySelector("#search-box");
+const countryInfo = document.querySelector(".country-info");
+const countryListDom = document.querySelector(".country-list");
+
+countryInput.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
+
+function fetchCountries(name) {
+    let countryName = name.target.value.trim();
+    let countryInfoReceived = fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+        .then((data) => data.json())
+        .then((result) => createDesign(result))
+}
+
+function createDesign(countryList) {
+    if (countryList.length === 1) {
+        return makeCountryCard(countryList[0])
+    } else if (countryList.length > 1 && countryList.length < 11) {
+        return makeCountriesList(countryList)
+    }
+
+}
+
+function makeCountryCard(country) {
+    let singleCardContent = `
+    <img src="${country.flags.svg}" alt="${country.name.official}" height="60px" width="100px"/>
+    <h1>${country.name.official}</h1>
+    <ul>
+        <li>Capital: ${country.capital}</li>
+        <li>Population: ${country.population}</li>
+        <li>Languages: ${Object.values(country.languages).join(", ")}</li>
+    </ul>`;
+    countryInfo.innerHTML = singleCardContent;
+    countryListDom.innerHTML = "";
+}
+
+function makeCountriesList(listmore) {
+    let toInclude = ""
+    listmore.forEach((country) => {
+        toInclude = toInclude + `<li>
+        <img src="${country.flags.svg}" alt="${country.name.official}" height="60px" width="100px"/>
+        <h1>${country.name.official}</h1>
+        </li>`
+    })
+    countryListDom.innerHTML = toInclude;
+    countryInfo.innerHTML = "";
+}
