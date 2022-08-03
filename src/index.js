2cde4cc9d685e4b9751af24502c63e1958ba7ed6
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-03 14:28:14 +0300

    Pre-final commit 03082022

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './css/styles.css';
-
 import debounce from 'lodash.debounce'
+import Notiflix from 'notiflix';
+// func import 
 
-const DEBOUNCE_DELAY = 900;
+const DEBOUNCE_DELAY = 300;
 
 const countryInput = document.querySelector("#search-box");
 const countryInfo = document.querySelector(".country-info");
@@ -12,9 +13,17 @@ countryInput.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY))
 
 function fetchCountries(name) {
     let countryName = name.target.value.trim();
-    let countryInfoReceived = fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
-        .then((data) => data.json())
-        .then((result) => createDesign(result))
+    if (countryName !== "") {
+        let countryInfoReceived = fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+            .then((data) => {
+                if (data.status === 404) {
+                    Notiflix.Notify.failure("Oops, there is no country with that name")
+                }
+                return data.json()
+            })
+            .then((result) => createDesign(result))
+            .catch((error) => console.log(error))
+    }
 }
 
 function createDesign(countryList) {
@@ -22,18 +31,19 @@ function createDesign(countryList) {
         return makeCountryCard(countryList[0])
     } else if (countryList.length > 1 && countryList.length < 11) {
         return makeCountriesList(countryList)
+    } else if (countryList.length > 10) {
+        Notiflix.Notify.info("Sorry, too much countries here");
     }
-
 }
 
 function makeCountryCard(country) {
     let singleCardContent = `
-    <img src="${country.flags.svg}" alt="${country.name.official}" height="60px" width="100px"/>
-    <h1>${country.name.official}</h1>
-    <ul>
-        <li>Capital: ${country.capital}</li>
-        <li>Population: ${country.population}</li>
-        <li>Languages: ${Object.values(country.languages).join(", ")}</li>
+    <img src="${country.flags.svg}" alt="${country.name.common}" height="60px" width="100px"/>
+    <h1>${country.name.common}</h1>
+    <ul style="list-style: none; padding-left: 10px">
+        <li><strong>Capital:</strong> ${country.capital}</li>
+        <li><strong>Population:</strong> ${country.population}</li>
+        <li><strong>Languages:</strong> ${Object.values(country.languages).join(", ")}</li>
     </ul>`;
     countryInfo.innerHTML = singleCardContent;
     countryListDom.innerHTML = "";
@@ -42,9 +52,8 @@ function makeCountryCard(country) {
 function makeCountriesList(listmore) {
     let toInclude = ""
     listmore.forEach((country) => {
-        toInclude = toInclude + `<li>
-        <img src="${country.flags.svg}" alt="${country.name.official}" height="60px" width="100px"/>
-        <h1>${country.name.official}</h1>
+        toInclude = toInclude + `<li style="padding: 3px; list-style: none">
+        <h4 style="margin: 1px"><img src="${country.flags.svg}" alt="${country.name.common}" height="24px" width="40px" style="display: inline-block; margin-right: 10px;"/>${country.name.common}</h4>
         </li>`
     })
     countryListDom.innerHTML = toInclude;
